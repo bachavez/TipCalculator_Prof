4e03442aed5acadaa15078799377b2e76ef01196
bachavez <bachavez@yahoo.com>
2019-02-06 00:37:49 -0800

    Final Changes

--- app/src/main/java/edu/miracosta/cs134/tipcalculator/MainActivity.java
+++ app/src/main/java/edu/miracosta/cs134/tipcalculator/MainActivity.java
@@ -2,11 +2,21 @@ package edu.miracosta.cs134.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    //member variable to format as currency
+    NumberFormat currency = NumberFormat.getCurrencyInstance(Locale.getDefault());
+    NumberFormat percent = NumberFormat.getPercentInstance(Locale.getDefault());
+    String billAmount = "0";
 
     // Member variables for each component used in the app
     private EditText amountEditText;
@@ -19,5 +29,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        amountEditText = findViewById(R.id.amountEditText);
+        percentTextView = findViewById(R.id.percentTextView);
+        tipTextView = findViewById(R.id.tipTextView);
+        totalTextView = findViewById(R.id.totalTextView);
+        percentSeekBar = findViewById(R.id.percentSeekBar);
+
+        //initialize link to the model
+
+        final Bill currentBill = new Bill();
+        //set tip percent to match the seek par
+
+        currentBill.setTipPercent(percentSeekBar.getProgress() / 100.0);
+
+        //connect code to the event on progress change for the seek bar
+
+        percentSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                //change the label of the tip percent
+
+                currentBill.setTipPercent(progress/100.0);
+                percentTextView.setText(percent.format(currentBill.getTipPercent()));
+                tipTextView.setText(currency.format(currentBill.getTipAmount()));
+                totalTextView.setText(currency.format(currentBill.getTotalAmount()));
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            //does nothing right now -- can't delete because you are implementing a interface
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        amountEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                billAmount = amountEditText.getText().toString();
+                if (billAmount.equals("")){
+                    billAmount = "0";
+                }
+                currentBill.setAmount(Double.parseDouble(billAmount));
+                tipTextView.setText(currency.format(currentBill.getTipAmount()));
+                totalTextView.setText(currency.format(currentBill.getTotalAmount()));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
 }
